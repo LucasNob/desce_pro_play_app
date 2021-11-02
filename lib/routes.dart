0a453d60b8d110d4dec35be86e885a5c5dff50e8
Matheus Lima de Almeida <matheuslima.tipi@gmail.com>
2021-11-02 18:40:35 -0300

    First commit - Cadastro Locais

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import 'package:desce_pro_play_app/views/forget_password_email_screen.dart';
 import 'package:desce_pro_play_app/views/forget_password_screen.dart';
 import 'package:desce_pro_play_app/views/home_login_screen.dart';
+import 'package:desce_pro_play_app/views/register_location_screen.dart';
 import 'package:desce_pro_play_app/views/register_screen.dart';
 import 'package:desce_pro_play_app/views/register_sports_screen.dart';
 import 'package:flutter/material.dart';
@@ -9,11 +10,11 @@ class AppRoutes {
   AppRoutes._();
 
   static const String home_login = '/home_login';
-  static const String register ='/register';
-  static const String register_sports ='/register_sports';
-  static const String forget_password_email ='/forget_password_email';
-  static const String forget_password ='/forget_password';
-
+  static const String register = '/register';
+  static const String register_sports = '/register_sports';
+  static const String forget_password_email = '/forget_password_email';
+  static const String forget_password = '/forget_password';
+  static const String register_location = '/register_location';
   static Map<String, WidgetBuilder> define() {
     return {
       home_login: (context) => HomeLoginScreen(),
@@ -21,6 +22,7 @@ class AppRoutes {
       register_sports: (context) => RegisterSportsScreen(),
       forget_password_email: (context) => ForgetPasswordEmailScreen(),
       forget_password: (context) => ForgetPasswordScreen(),
+      register_location: (context) => RegisterLocationScreen(),
     };
   }
 }
---
+++ lib/views/register_location_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class RegisterLocationScreen extends StatefulWidget {
+  const RegisterLocationScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegisterLocationScreenState createState() => _RegisterLocationScreenState();
+}
+
+class _RegisterLocationScreenState extends State<RegisterLocationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _taxaController = TextEditingController();
+  TextEditingController _enderecoController = TextEditingController();
+  TextEditingController _contatoController = TextEditingController();
+  TextEditingController _cepController = TextEditingController();
+  TextEditingController _sobreController = TextEditingController();
+  String dropdownValue = "Selecionar";
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    final fieldFontSize = mediaQuery.size.width / 24;
+    final buttonFontSize = mediaQuery.size.width / 14;
+    final topAndBottomPadding = mediaQuery.size.height / 30;
+
+    final logo = Material(
+      color: Colors.transparent,
+      child: Image.asset(
+        "lib/resources/logo.png",
+        height: mediaQuery.size.height / 4,
+        width: mediaQuery.size.width / 1.5,
+      ),
+    );
+
+    Widget textField(
+        TextEditingController controller, String nome, double width) {
+      return Material(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(10),
+          child: Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Container(
+              width: mediaQuery.size.width / width,
+              child: TextFormField(
+                controller: controller,
+                maxLines: 1,
+                style: GoogleFonts.anton(
+                    fontSize: fieldFontSize, color: Colors.white),
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: nome,
+                ),
+              ),
+            ),
+          ));
+    }
+
+    Widget dropdownField(double width) {
+      return Material(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(10),
+          child: Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Container(
+              width: mediaQuery.size.width / width,
+              child: DropdownButton<String>(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_downward),
+                iconSize: 24,
+                elevation: 16,
+                style: GoogleFonts.anton(
+                    color: Colors.white, fontSize: fieldFontSize),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                },
+                items: <String>['Selecionar', 'Two', 'Free', 'Four']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value, style: TextStyle(color: Colors.black54)),
+                  );
+                }).toList(),
+              ),
+            ),
+          ));
+    }
+
+    Widget registerContainer() {
+      return Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildTopPadding(10, textField(_nameController, "Nome", 3)),
+              buildTopPadding(
+                  10, textField(_taxaController, "Taxa de Entrada", 3)),
+            ],
+          ),
+          buildTopPadding(10, textField(_enderecoController, "Endereço", 1.1)),
+          buildTopPadding(10, textField(_contatoController, "Contato", 1.1)),
+          buildTopPadding(10, textField(_cepController, "CEP", 1.1)),
+          buildTopPadding(10, dropdownField(1.1)),
+          buildTopPadding(10, textField(_sobreController, "Sobre", 1.1)),
+        ],
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          leading: null,
+          title: Text(
+            "Cadastro de Local",
+          ),
+          backgroundColor: Color(0xffFF8A00),
+        ),
+        backgroundColor: Colors.white,
+        body: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+                padding: EdgeInsets.only(
+                    top: mediaQuery.size.height / 12,
+                    bottom: mediaQuery.size.height / 12),
+                child: registerContainer())));
+  }
+
+  Padding buildTopPadding(double topPadding, Widget field) {
+    return Padding(
+      padding: EdgeInsets.only(top: topPadding),
+      child: field,
+    );
+  }
+}
