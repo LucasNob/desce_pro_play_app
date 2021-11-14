39acc04ea4bc3b0bd809ebf41f023d63304eb198
Matheus Lima de Almeida <matheuslima.tipi@gmail.com>
2021-11-14 17:11:55 -0300

    Corrigindo rota

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:desce_pro_play_app/routes.dart';
 import 'package:desce_pro_play_app/views/home_login_screen.dart';
-import 'package:desce_pro_play_app/views/register_location_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -16,8 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Desce Pro Play',
       routes: AppRoutes.define(),
-      // alterar antes de subir a versão final
-      home: RegisterLocationScreen(),
+      home: HomeLoginScreen(),
     );
   }
 }
