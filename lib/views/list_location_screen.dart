95f3f8f0481001deae2f150454d9fd11d3f72fb0
Sérgio Vicente T <sergiovicente_tavuenc@outlook.com>
2021-11-25 20:28:15 -0300

    Removed unused library

--- lib/views/list_location_screen.dart
+++ lib/views/list_location_screen.dart
@@ -1,3 +1,4 @@
+import 'package:desce_pro_play_app/views/location_profile_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
