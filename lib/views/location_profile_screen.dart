79db17754f073b4e51050f92e2e5494de6c2c1f5
Sérgio Vicente T <sergiovicente_tavuenc@outlook.com>
2021-11-26 15:49:34 -0300

    Participate button

--- lib/views/location_profile_screen.dart
+++ lib/views/location_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,8 @@ import 'package:google_fonts/google_fonts.dart';
 import '../routes.dart';
 
 class LocationProfileScreen extends StatefulWidget {
-  const LocationProfileScreen({Key? key,required this.locationName}) : super(key: key);
+  const LocationProfileScreen({Key? key, required this.locationName})
+      : super(key: key);
 
   final String locationName;
   @override
@@ -14,13 +16,55 @@ class LocationProfileScreen extends StatefulWidget {
 }
 
 class _LocationProfileViewState extends State<LocationProfileScreen> {
+  User? currentUser = FirebaseAuth.instance.currentUser;
+  CollectionReference userdata =
+      FirebaseFirestore.instance.collection('userdata');
+  DocumentReference? userDocRef;
+  DocumentSnapshot? userDoc;
+
+  CollectionReference locationdata =
+      FirebaseFirestore.instance.collection('locationdata');
+  DocumentReference? locationDocRef;
+  DocumentSnapshot? locationDoc;
+
+  List? users;
+  String? user;
 
   Text buildText(String text, fontSize, color) {
     return Text(text,
         style: GoogleFonts.anton(fontSize: fontSize, color: color));
   }
-  CollectionReference locationdata
-    = FirebaseFirestore.instance.collection('locationdata');
+
+  Future getName() async {
+    userDocRef = userdata.doc(currentUser!.email);
+    userDoc = await userDocRef!.get();
+    setState(() {
+      user = userDoc!.get('first_name') + " " + userDoc!.get('last_name');
+      print(user);
+    });
+  }
+
+  void addUser() async {
+    locationDoc = await locationDocRef!.get();
+    users = locationDoc!.get("users");
+    await getName();
+
+    if (!users!.contains(user)) {
+      users!.add(user);
+      locationDocRef!.update({'users': users});
+    }
+  }
+
+  void removeUser() async {
+    locationDoc = await locationDocRef!.get();
+    users = locationDoc!.get("users");
+    await getName();
+
+    if (users!.contains(user)) {
+      users!.remove(user);
+      locationDocRef!.update({'users': users});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,38 +85,52 @@ class _LocationProfileViewState extends State<LocationProfileScreen> {
             return Text("Dados de local inexistente");
           }
           if (snapshot.connectionState == ConnectionState.done) {
-            Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+            Map<String, dynamic> data =
+                snapshot.data!.data() as Map<String, dynamic>;
 
             final imgURL = data['image_url'];
+
+            locationDocRef = locationdata.doc('${data['name']}');
 
             return Container(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.network(
-                    imgURL,
-                    height: mediaQuery.size.height/5,
-                    width: mediaQuery.size.width,
-                  ),
+                  // Material(
+                  //     child: imgURL != null
+                  //         ? ClipOval(
+                  //             child: Image.network(
+                  //               imgURL,
+                  //               width: mediaQuery.size.width / 2.5,
+                  //               height: mediaQuery.size.height / 4.75,
+                  //               fit: BoxFit.cover,
+                  //             ),
+                  //           )
+                  //         : ClipOval(
+                  //             child: Image.asset(
+                  //               "lib/resources/user_default_profile_image.png",
+                  //               width: mediaQuery.size.width / 2.5,
+                  //               height: mediaQuery.size.height / 4.75,
+                  //               fit: BoxFit.fill,
+                  //             ),
+                  //           )),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       buildText("Nome", labelFontSize, Colors.grey),
-                      buildText(
-                          '${data['name']}', valueFontSize, Colors.black),
-                      buildText(
-                          "${data['about']}", valueFontSize, Colors.black)
+                      buildText('${data['name']}', valueFontSize, Colors.black),
+                      buildText("${data['about']}", valueFontSize, Colors.black)
                     ],
                   ),
                   Padding(
                       padding:
-                      EdgeInsets.only(top: mediaQuery.size.height / 35),
+                          EdgeInsets.only(top: mediaQuery.size.height / 35),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           buildText("Endereço", labelFontSize, Colors.grey),
-                          buildText("${data['adress']}", valueFontSize,
-                              Colors.black)
+                          buildText(
+                              "${data['adress']}", valueFontSize, Colors.black)
                         ],
                       )),
                   Padding(
@@ -81,8 +139,7 @@ class _LocationProfileViewState extends State<LocationProfileScreen> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         buildText("CEP", labelFontSize, Colors.grey),
-                        buildText("${data['cep']}", valueFontSize,
-                            Colors.black)
+                        buildText("${data['cep']}", valueFontSize, Colors.black)
                       ],
                     ),
                   ),
@@ -104,7 +161,70 @@ class _LocationProfileViewState extends State<LocationProfileScreen> {
           return Text("loading...");
         });
 
+    final participateButton = ElevatedButton(
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                mediaQuery.size.width / 20,
+                mediaQuery.size.height / 150,
+                mediaQuery.size.width / 20,
+                mediaQuery.size.height / 150),
+            child: Text(
+              "participar".toUpperCase(),
+              style: GoogleFonts.anton(
+                  fontSize: buttonFontSize, color: Colors.black),
+            )),
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Color(0xff5CD032)),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ))),
+        onPressed: () {
+          addUser();
+          print("participar");
+        });
 
+    final exitButton = ElevatedButton(
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                mediaQuery.size.width / 20,
+                mediaQuery.size.height / 150,
+                mediaQuery.size.width / 20,
+                mediaQuery.size.height / 150),
+            child: Text(
+              "sair".toUpperCase(),
+              style: GoogleFonts.anton(
+                  fontSize: buttonFontSize, color: Colors.black),
+            )),
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.red),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ))),
+        onPressed: () {
+          removeUser();
+        });
+
+    final bodyContainer = Container(
+      child: Column(
+        children: <Widget>[
+          loadProfile,
+          Padding(
+            padding: EdgeInsets.only(
+                top: topAndBottomPadding, bottom: topAndBottomPadding),
+            child: Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(right: mediaQuery.size.width / 10),
+                    child: participateButton),
+                exitButton
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
 
     return Scaffold(
       appBar: AppBar(
@@ -123,14 +243,13 @@ class _LocationProfileViewState extends State<LocationProfileScreen> {
         ),
       ),
       body: SingleChildScrollView(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: topAndBottomPadding),
-                  //child: bodyContainer,
-                  child: loadProfile,
-                ),
-              ])),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Padding(
+          padding: EdgeInsets.only(top: topAndBottomPadding),
+          //child: bodyContainer,
+          child: bodyContainer,
+        ),
+      ])),
     );
   }
 }
